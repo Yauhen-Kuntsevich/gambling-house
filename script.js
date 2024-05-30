eea0ae3d2c6bcf51813a89f619d0980055552db8
Yauheni Kuntsevich <jauhien.k@gmail.com>
2024-05-30 15:42:50 +0200

    Script for burger-icon managment was created

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const languageButton = document.querySelector(".language-button");
 
+const mobileBurgerButton = document.querySelector(".mobile-burger-button");
+const burgerLines = document.querySelectorAll(".burger-line");
+const mobileCrossIcon = document.querySelector(".mobile-cross-icon");
+
 languageButton.addEventListener("click", () => {
   languageButton.innerHTML =
     languageButton.textContent === "EN"
       ? '<span class="icon"></span>RU'
       : '<span class="icon"></span>EN';
+});
+
+mobileBurgerButton.addEventListener("click", () => {
+  if (burgerLines[0].style.display === "none") {
+    burgerLines.forEach((line) => {
+      line.style.display = "inline";
+    });
+    mobileCrossIcon.style.display = "none";
+  } else {
+    burgerLines.forEach((line) => {
+      line.style.display = "none";
+    });
+    mobileCrossIcon.style.display = "inline";
+  }
 });
